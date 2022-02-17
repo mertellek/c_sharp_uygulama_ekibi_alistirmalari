6a1ce0fd7970907dbefc59a7bf7d8ae91f740196
Mert Ellek <mertellek@hotmail.com>
2022-02-17 13:59:34 +0300

    hafta_1

---
+++ hafta_1_alistirma_3/hafta_1_alistirma_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace hafta_1_alistirma_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dikdörtgenin alanını ve çevresini hesaplayarak ekranda gösteren program
+            Console.Write("Kısa Kenar : ");
+            int k_kenar = int.Parse(Console.ReadLine());
+            Console.Write("Uzun Kenar : ");
+            int u_kenar = int.Parse(Console.ReadLine());
+            Console.WriteLine("Alan : " + (u_kenar * k_kenar));
+            Console.WriteLine("Çevre : " + (u_kenar + k_kenar) * 2);
+            Console.ReadKey();
+        }
+    }
+}

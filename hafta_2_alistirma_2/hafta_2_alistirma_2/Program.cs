119412f3d68c145c24f6672d8100420d3109aedd
Mert Ellek <mertellek@hotmail.com>
2022-02-17 13:50:31 +0300

    hafta_1

---
+++ hafta_2_alistirma_2/hafta_2_alistirma_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace hafta_2_alistirma_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kullanıcıdan 2 sayı isteyerek bu sayıların toplamını bulan program
+            Console.Write("1. Sayı : ");
+            int sayi1 = int.Parse(Console.ReadLine());
+            Console.Write("2. Sayı : ");
+            int sayi2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(sayi1 + " ve " + sayi2 + " Sayısının Toplamı : " + (sayi1 + sayi2));
+            Console.ReadKey();
+        }
+    }
+}

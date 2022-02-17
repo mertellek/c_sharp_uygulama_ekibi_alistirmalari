796ab2a8c087a7932c5d681de00dc978a098c169
Mert Ellek <mertellek@hotmail.com>
2022-02-17 14:03:52 +0300

    hafta_1

---
+++ hafta_1_alistirma_4/hafta_1_alistirma_4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace hafta_1_alistirma_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Girilen sayının tek mi çift mi olduğunu bulan program.
+            Console.Write("Sayı giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+            
+            if (sayi%2==0) Console.WriteLine(sayi + " sayısı çifttir.");
+            else Console.WriteLine(sayi + " sayısı tektir.");
+            Console.ReadKey();
+        }
+    }
+}

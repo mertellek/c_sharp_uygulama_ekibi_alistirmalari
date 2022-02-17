86e02bdac77f8e14f7315f1544b13254b62e7aa4
Mert Ellek <mertellek@hotmail.com>
2022-02-17 13:03:52 +0300

    hafta_1

---
+++ hafta_1_alistirma_1/hafta_1_alistirma_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace hafta_1_alistirma_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ad ve soyad bilgisini string değişkenlerine alarak ekrana yazdıran program.
+            Console.Write("Adınız : ");
+            string name = Console.ReadLine();
+            Console.Write("Soyadınız : ");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Merhaba " + name + " " + surname);
+            Console.ReadKey();
+        }
+    }
+}

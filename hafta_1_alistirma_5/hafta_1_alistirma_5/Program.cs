88293174085f12c60d5d78cf4180aede2c9cd264
Mert Ellek <mertellek@hotmail.com>
2022-02-17 14:08:48 +0300

    hafta_1

---
+++ hafta_1_alistirma_5/hafta_1_alistirma_5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace hafta_1_alistirma_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Girilen sayının 0-100 arasında olup olmadığını kontrol eden program.
+            Console.Write("Sayı giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+            if (sayi <= 100 && sayi >= 0) Console.WriteLine(sayi + " sayısı 0-100 aralığındadır.");
+            else Console.WriteLine(sayi + " sayısı 0-100 aralığında değildir.");
+            Console.ReadKey();
+        }
+    }
+}

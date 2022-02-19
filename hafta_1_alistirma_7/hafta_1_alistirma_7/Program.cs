9eb96d4cf749cd1f85544bd82eaa6f1446581353
Mert Ellek <mertellek@hotmail.com>
2022-02-19 19:55:13 +0300

    hafta_1

---
+++ hafta_1_alistirma_7/hafta_1_alistirma_7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace hafta_1_alistirma_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cümledeki harf sayısını bulan program.
+            Console.Write("Lütfen metni girin: ");
+            string metin = Console.ReadLine();
+            //metin = metin.Trim().Replace(" ", string.Empty); //metindeki tüm boşlukları temizler
+            string harfler = "ABCÇDEFGĞHIİJKLMNOÖQPRŞTUÜVWXYZabcçdefgğhıijklmnoöqprsştuüvwxyz";
+            int sayac = 0;
+            for (int i = 0; i < metin.Length; i++)
+            {
+                if (harfler.Contains(metin[i]))
+                {
+                    sayac++;
+                }
+            }
+            Console.WriteLine("Girmiş olduğunuz ifadede " + sayac + " harf bulunuyor.");
+            Console.ReadKey();
+        }
+    }
+}

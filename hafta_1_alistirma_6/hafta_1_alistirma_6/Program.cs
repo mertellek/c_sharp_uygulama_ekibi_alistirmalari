ee95b0fd689145a10c16766aa6a402579e1c7610
Mert Ellek <mertellek@hotmail.com>
2022-02-19 19:54:49 +0300

    hafta_1

---
+++ hafta_1_alistirma_6/hafta_1_alistirma_6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace hafta_1_alistirma_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Vücut Kitle İndeksi hesaplayan program.
+            Console.Write("Lütfen boyunuzu metre cinsinden arada virgül olacak şekilde giriniz: ");
+            double boy = double.Parse(Console.ReadLine());
+            Console.Write("Lütfen kilonuzu kilogram cinsinden giriniz: ");
+            double kilo = double.Parse(Console.ReadLine());
+            Console.WriteLine("Vücut Kitle Endeksiniz: " + (kilo / (boy * boy)));
+            Console.ReadKey();
+        }
+    }
+}

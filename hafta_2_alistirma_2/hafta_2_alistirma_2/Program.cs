5e7380c3d23fb1ef89aa941b106d956659b4943a
Mert Ellek <mertellek@hotmail.com>
2022-02-23 19:51:07 +0300

    hafta_2

---
+++ hafta_2_alistirma_2/hafta_2_alistirma_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace hafta_2_alistirma_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 - N arası 3'e ve 7' ye tam bölünen sayıları listeleme
+            Console.Write("Lütfen N sayısını giriniz: ");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++)
+            {
+                if (i%3==0 && i%7==0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}

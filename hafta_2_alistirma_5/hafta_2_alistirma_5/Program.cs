b4528285b94d5fcf7b10b87b5c6fccd841256653
Mert Ellek <mertellek@hotmail.com>
2022-02-24 15:53:01 +0300

    hafta_2

---
+++ hafta_2_alistirma_5/hafta_2_alistirma_5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace hafta_2_alistirma_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kullanıcıdan 10 adet sayı alarak bu sayıların toplamını ekranda gösteren program
+            int[] sayilar = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("{0}. Sayıyı giriniz: ",i+1);
+                sayilar[i] = int.Parse(Console.ReadLine());
+            }
+            int toplam = 0;
+            foreach (var item in sayilar)
+            {
+                toplam += item;
+            }
+            Console.WriteLine("Girmiş olduğunuz sayıların toplamı= " + toplam);
+            Console.ReadKey();
+        }
+    }
+}

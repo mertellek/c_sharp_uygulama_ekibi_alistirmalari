7620fe83affd5d84cd8a3920aba52ca5b81adeb6
Mert Ellek <mertellek@hotmail.com>
2022-02-24 15:52:30 +0300

    hafta_2

---
+++ hafta_2_alistirma_3/hafta_2_alistirma_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace hafta_2_alistirma_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Diziyi alfabetik olarak sıralama(Kullanıcının gireceği kelimelerden oluşan diziyi alfabetik olarak sıralama)
+            Console.WriteLine("Lütfen kelimeleri aralarında birer boşluk olacak şekilde giriniz: ");
+            string metin = Console.ReadLine();
+            string[] metinler = metin.Split(" "); //"metin" ifadesini boşluktan itibaren ayırıp "metinler" dizisine aktarır
+            Array.Sort(metinler);
+            foreach (var item in metinler)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}

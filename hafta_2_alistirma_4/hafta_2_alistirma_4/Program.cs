fbd8cb8d1886ce898a8a3bcc369ed11b76ecd85c
Mert Ellek <mertellek@hotmail.com>
2022-02-24 15:52:45 +0300

    hafta_2

---
+++ hafta_2_alistirma_4/hafta_2_alistirma_4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace hafta_2_alistirma_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Girilen metindeki sesli harfleri bulan program
+            Console.Write("Metni giriniz: ");
+            string metin = Console.ReadLine();
+            metin = metin.Trim().Replace(" ", string.Empty);
+            for (int i = 0; i < metin.Length; i++)
+            {
+                if (metin[i] == 'a' || metin[i] == 'e' || metin[i] == 'ı' || metin[i] == 'i' || 
+                    metin[i] == 'o' || metin[i] == 'ö' || metin[i] == 'u' || metin[i] == 'ü' || 
+                    metin[i] == 'A' || metin[i] == 'E' || metin[i] == 'I' || metin[i] == 'İ' || 
+                    metin[i] == 'O' || metin[i] == 'Ö' || metin[i] == 'U' || metin[i] == 'Ü')
+                {
+                    Console.WriteLine("Metnin " + (i+1) + ". elemanı: " + metin[i]);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}

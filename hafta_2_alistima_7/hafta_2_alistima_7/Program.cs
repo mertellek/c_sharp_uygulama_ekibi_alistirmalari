5a987edf3a38079d023361b52cf464a60e9fbb8e
Mert Ellek <mertellek@hotmail.com>
2022-02-25 15:10:15 +0300

    hafta_2

---
+++ hafta_2_alistima_7/hafta_2_alistima_7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace hafta_2_alistima_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kullanıcının girdiği metni kullanıcının girdiği kadar ekranda yazdıran program
+            Console.Write("Lütfen yazdırmak istediğiniz metni giriniz: ");
+            string metin = Console.ReadLine();
+            Console.Write("Kaç adet yazdırmak istediğiniz giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+            for (int i = 0; i < sayi; i++)
+            {
+                Console.WriteLine(metin);
+            }
+            Console.ReadKey();
+        }
+    }
+}
